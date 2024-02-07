069920355faec788a3099d46574c8cb0e7f6e50f
Joaquim G. de Souza <jocaq124@gmail.com>
2024-02-07 16:56:52 -0300

    aprendendo

---
+++ 03-SomandoTextos.js
@@ -0,0 +1,9 @@
+var nome = "Joaquim";
+var boasVindas = "Olá " + nome + ", seja bem vindo";
+console.log(boasVindas);
+
+var texto1 = "Olá";
+var texto2 = "Bom dia"
+
+console.log(texto1 + texto2);
+console.log(texto1 + ", " + texto2);
---
+++ 04-operadores.js
@@ -0,0 +1,17 @@
+var x = 22;
+var y = 5;
+console.log("x =", x,"| y =",y);
+
+var resultado = x + y - 3;
+console.log(resultado);
+
+var resultado = x + y * 3;
+console.log(resultado);
+
+var resultado = (x+y)*3;
+console.log(resultado);
+
+var resultado = (x+y)/ 3;
+console.log(resultado);
+
+var resultado = (x/2 + 3*y + (x-10))/2;
