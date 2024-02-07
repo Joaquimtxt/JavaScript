7eb2b9f3de12d9fd31ec988198703f173281f279
Joaquim G. de Souza <jocaq124@gmail.com>
2024-02-07 15:06:07 -0300

    Horas

---
+++ 02.6-relogio.js
@@ -0,0 +1,7 @@
+var Data = new Date();
+var Horas = Data.getHours();
+var Minutos = Data.getMinutes();
+var Segundos = Data.getSeconds();
+var Milissegundos = Data.getMilliseconds();
+console.log("Data:",Data);
+console.log("Horário", Horas,":", Minutos,":", Segundos,":", Milissegundos);
