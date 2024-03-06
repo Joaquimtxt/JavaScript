48db11c1d803a228ff9c0cb15799e9ae5d1583fa
Joaquim G. de Souza <jocaq124@gmail.com>
2024-03-06 16:31:59 -0300

    Jogo da velha

--- 18-while.js
+++ 18-while.js
@@ -5,43 +5,73 @@
 //   console.log(contador);
 // }
 
-var countdown = 5;
+// var countdown = 5;
 
-while (countdown >= 0) {
-  console.log(countdown);
-  countdown--;
+// while (countdown >= 0) {
+//   console.log(countdown);
+//   countdown--;
+// }
+// console.log(`Lançamento!`);
+
+// let contador = 0;
+
+// while (contador <= 5) {
+//   console.log(contador);
+//   contador++;
+// }
+
+// //Estourando o limite do cartão - versão while
+// var limite = 500;
+// var fatura = 0;
+
+// while (fatura <= limite) {
+//     let gasto = Math.random() * 100
+//     fatura += gasto
+//     console.log(`Gastei R$ ${gasto.toFixed(2)}------ Fatura: R$ ${fatura.toFixed(2)}`);
+//     fatura > limite && console.log(`Estourou o limite do cartão`)
+
+// }
+
+// //Estourando o limite do cartão - versão do while
+// console.log("__________________________________________________________")
+// var limite = 500;
+// var fatura = 0;
+
+// do {
+//     let gasto = Math.random() * 100
+//     fatura += gasto
+//     console.log(`Gastei R$ ${gasto.toFixed(2)}------ Fatura: R$ ${fatura.toFixed(2)}`);
+//     fatura > limite && console.log(`Estourou o limite do cartão`)
+
+// } while (fatura <= limite);
+
+//Jogo de cara e coroa - melhor de três tentativas competição - 3 var cara ou coroa -> aleatória
+//placar - vitórias e derrotas
+
+var round = 1;
+var moeda = "";
+var escolhaJogador = "";
+var vitoria = 0;
+var derrota = 0;
+
+while (round <= 3) {
+  moeda = Math.random() >= 0.5 ? "Cara" : "Coroa"//Se Math.random for maior ou igual a 0.5, cai cara, se não, Coroa
+  escolhaJogador = Math.random() >= 0.5 ? "Cara" : "Coroa";//A escolha do Jogador é pra na frente fazer a comparação
+
+  if (escolhaJogador == moeda) { //Aqui está sendo feita a comparação, se for igual vai dar vitória pro jogador
+    console.log(`Parabéns, você ganhou!`);
+    vitoria++;
+  } else {
+    console.log(`Que pena, você perdeu!`);
+    derrota++; // Se não dá derrota
+  }
+  console.log(moeda + " | " + escolhaJogador);
+  round++;
 }
-console.log(`Lançamento!`);
 
-let contador = 0;
-
-while (contador <= 5) {
-  console.log(contador);
-  contador++;
-}
-
-//Estourando o limite do cartão - versão while
-var limite = 500;
-var fatura = 0;
-
-while (fatura <= limite) {
-    let gasto = Math.random() * 100
-    fatura += gasto
-    console.log(`Gastei R$ ${gasto.toFixed(2)}------ Fatura: R$ ${fatura.toFixed(2)}`);
-    fatura > limite && console.log(`Estourou o limite do cartão`)
-
-}
-
-//Estourando o limite do cartão - versão do while
-console.log("__________________________________________________________")
-var limite = 500;
-var fatura = 0;
-
-do {
-    let gasto = Math.random() * 100
-    fatura += gasto
-    console.log(`Gastei R$ ${gasto.toFixed(2)}------ Fatura: R$ ${fatura.toFixed(2)}`);
-    fatura > limite && console.log(`Estourou o limite do cartão`)
-
-
-} while (fatura <= limite);
+console.log(`-----------------------------------`);
+console.log(`             Placar                      `);
+console.log(`-----------------------------------`);
+console.log(`Vitórias: ${vitoria}`);
+console.log(`Derrotas: ${derrota}`);
+console.log(`${vitoria > derrota ? "Você ganhou!" : "Você perdeu!"}`);
---
+++ Atividade07.js
@@ -0,0 +1,170 @@
+// var populacaoA = Math.random() * 30000;
+// var populacaoB = Math.random() * 100000;
+// const porcentagemA = 0.03;
+// const porcentagemB = 0.015;
+// var anos = 0;
+// console.log(
+//   Math.round(populacaoA) +
+//     "= População A | População B = " +
+//     Math.round(populacaoB)
+// );
+// while (populacaoA < populacaoB) {
+//   populacaoA += populacaoA * porcentagemA;
+//   populacaoB += populacaoB * porcentagemB;
+
+//   console.log(
+//     `População A --> ${Math.round(
+//       populacaoA
+//     )} pessoas | População B --> ${Math.round(populacaoB)} pessoas`
+//   );
+
+//   anos++;
+//   if (populacaoB <= populacaoA) {
+//     console.log(
+//       `Depois de ${anos} anos, o País A terá população maior que o País B`
+//     );
+//     break;
+//   }
+// }
+// console.log(`-----------------------------------`);
+// console.log(`        Correção da atividade      `);
+// console.log(`-----------------------------------`);
+// //Correção
+// var popA = 100003;
+// var popB = 300004;
+// var ano = 0;
+
+// console.log(`PopA = ${popA} PopB =${popB}`);
+
+// while (popA < popB) {
+//   ano++;
+//   popA *= 1.03;
+//   popB *= 1.015;
+// }
+// console.log(
+//   `A população A (${Math.round(popA)}) irá passar a população B (${Math.round(
+//     popB
+//   )}) em ${ano} anos!`
+// );
+
+// console.log(`-----------------------------------`);
+// console.log(`-----------------------------------`);
+// console.log(`-----------------------------------`);
+// console.log(`-----------------------------------`);
+// console.log(`-----------------------------------`);
+// console.log(`      JOGO DA VELHA RANDÔMICO      `);
+
+// console.log(`_____|_____|_____`);
+// console.log(`_____|_____|_____`);
+// console.log(`     |     |     `);
+
+var jogo = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+let jogadorX = "X";
+let jogadorO = "O";
+
+function jogar(jogador) {
+  let linha, coluna;
+  do {
+    linha = Math.floor(Math.random() * 3);
+    coluna = Math.floor(Math.random() * 3);
+  } while (jogo[linha][coluna] !== "");
+  jogo[linha][coluna] = jogador;
+}
+
+for (var i = 0; i < 9; i++) {
+  if (i % 2 == 0) {
+    jogar(jogadorX);
+    console.table(jogo);
+  } else {
+    jogar(jogadorO);
+    console.table(jogo);
+  }
+}
+
+console.table(jogo);
+
+for (var i = 0; i < 3; i++) {
+  switch (true) {
+    case jogo[i][0] == jogo[i][1] && jogo[i][1] == jogo[i][2]: {
+      if (jogo[i][0] == "X") {
+        console.log(`O Jogador X venceu`);
+        return
+      } else {
+        console.log(`O Jogador O venceu`);
+        return
+      }
+    }
+    case jogo[0][i] == jogo[1][i] && jogo[1][i] == jogo[2][i]: {
+      if (jogo[0][i] == "X") {
+        console.log(`O Jogador X venceu`);
+        return
+
+      } else {
+        console.log(`O Jogador O venceu`);
+        return
+
+      }
+    }
+    case jogo[0][0] == jogo[1][1] && jogo[1][1] == jogo[2][2]: {
+      if (jogo[i][i] === "X") {
+        console.log(`O Jogador X venceu`);
+        return
+
+      } else {
+        console.log(`O Jogador O venceu`);
+        return
+
+      }
+    }
+    case jogo[0][2] == jogo[1][1] && jogo[1][i] == jogo[2][0]: {
+      if (jogo[i][i] === "X") {
+        console.log(`O Jogador X venceu`);
+        return
+
+      } else {
+        console.log(`O Jogador O venceu`);
+        return
+
+      }
+      break;
+    }
+    default: {
+      console.log("Deu Empate");
+      return
+
+    }
+  }
+}
+
+// for (let i = 0; i <= jogo.length; i++) {
+//   jogar(jogadorX);
+//   jogar(jogadorO);
+//         break;
+//     }
+//     switch (jogo) {
+//       case [0,0][0,1][0,2] == "X":
+//         case [0,0][1,1][2,2] == "X":
+//           case [0,0][1,0][2,0] == "X":
+//             case [0,1][1,1][1,2] == "X":
+//               case [0,2][1,2][2,2] == "X":
+//                 case [1,0][1,1][1,2] == "X":
+//                   case [2,0][2,1][2,2] == "X":
+//                 console.log("Você ganhou!");
+//                 case [0,0][0,1][0,2] == "O":
+//                   case [0,0][1,1][2,2] == "O":
+//                     case [0,0][1,0][2,0] == "O":
+//                       case [0,1][1,1][1,2] == "O":
+//                         case [0,2][1,2][2,2] == "O":
+//                           case [1,0][1,1][1,2] == "O":
+//                             case [2,0][2,1][2,2] == "O":
+//                               console.log("Você perdeu");
+//         break;
+
+//       default:
+//         console.log("Empate")
+//     }
+// console.table(jogo);
